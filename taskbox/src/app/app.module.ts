94f40b3fcd4aea7ab58155c0ca0a845f44927adc
Adam Samko <samko@alfabase.sk>
2021-12-29 20:17:31 +0100

    added data management

--- taskbox/src/app/app.module.ts
+++ taskbox/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task/task.component';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { TaskModule } from './components/task/task.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
